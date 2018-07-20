69b811c057c8bdb626ca35bec5ab4996874122e0
Matic Leva <matic.leva@inova.si>
2018-07-20 13:50:08 +0200

    Prepare follow up to cast

--- src/spells.ts
+++ src/spells.ts
@@ -1,7 +1,7 @@
 const spells = [
     {
         id: 'energy beam',
-        instructions: 'Everybody in 90 feet radius make dexterity saving throw!',
+        instructions: 'Everybody in 90 feet radius make dexterity saving throw! Did you succeeded?',
         action: (success: boolean) => {
             return success ? 'Nice, nothing happens!' : 'Take 1d8 damage!'
         }
