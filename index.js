a5837a2da574532f4c74516e13ddd0a6416b4eb5
Matic Leva <matic.leva@inova.si>
2018-07-20 12:29:03 +0200

    Tets

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
-  ctx.body = 'Hello World';
+  ctx.body = 'Welcome to Book of knowledge!';
 });
 
 if (!module.parent) app.listen(3596);
