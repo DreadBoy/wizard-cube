496d8b861483b32ad83f318aa14d1bb07cb9db17
Matic Leva <vertical3life@gmail.com>
2018-08-09 14:01:16 +0200

    Change die from number to entity

--- build/intents.js
+++ build/intents.js
@@ -17,15 +17,16 @@ const handlers = [
         id: 'projects/wizard-cube/agent/intents/38b83d78-9372-41b5-b18e-90b5cbd802c0',
         handler: (body) => {
             const { parameters } = body.queryResult;
-            const { number, die } = parameters;
+            const { number } = parameters;
+            const die = parseInt(parameters.die.slice(1)); // Die comes as D8, not only number
             let { modifier } = parameters;
             if (!number)
                 return {
-                    fulfillmentText: 'I didn\'t catch number of dice, repeat, please!',
+                    fulfillmentText: 'How many dice was it?',
                 };
             if (!die)
                 return {
-                    fulfillmentText: 'I didn\'t catch a die, repeat, please!',
+                    fulfillmentText: 'What die was it?',
                 };
             modifier = modifier || 0;
             const sum = new Array(number).fill(0).map(() => random(1, die)).reduce((acc, curr) => acc + curr, 0) + modifier;
